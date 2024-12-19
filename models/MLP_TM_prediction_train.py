a9fd20f9d3855f6f1b970ef81e137e68a65e0301
AVIP-laboratory <avip00316@gmail.com>
2024-12-19 16:46:17 +0900

    Update MLP_TM_prediction_train.py

--- models/MLP_TM_prediction_train.py
+++ models/MLP_TM_prediction_train.py
@@ -1,1 +1,24 @@
+# Import required libraries
+import pandas as pd
+from tensorflow.keras.models import Sequential
+from keras.layers import Dense
+from sklearn.metrics import r2_score
 
+# Load input and output data from CSV files
+input = pd.read_csv('file directory/geometric_parameters_train.csv', header=None)# Input data with 3 features
+output = pd.read_csv('file directory/simulated_TM_results.csv', header=None)# Output data with 41 features
+
+model = Sequential()
+model.add(Dense(5, input_dim=3, kernel_initializer='he_uniform', activation='relu'))
+model.add(Dense(11, kernel_initializer='he_uniform', activation='relu'))
+model.add(Dense(41, activation='linear'))
+model.compile(loss='mse', optimizer='adam', metrics=['accuracy', 'mse'])
+
+history = model.fit(input, output, epochs=100, batch_size=5)
+model.summary()
+
+model.save('file directory/MLP_TM_predictor.h5')
+
+Y_pred = model.predict(input)
+R2_score= r2_score(output, Y_pred)
+print(R2_score)
